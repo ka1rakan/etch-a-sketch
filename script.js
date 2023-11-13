764ee11d579de3a3cd60983583ecdeeb56107b9d
ka1rakan <hsuhankaraca@gmail.com>
2023-11-13 23:40:55 +0300

    add mouseover event listener to divs

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ for(let i=0; i<256; i++){
     const div = document.createElement("div");
     container.appendChild(div);
 }
+
+const dividers = document.querySelectorAll("#container div")
+dividers.forEach((div)=>{
+    div.addEventListener("mouseover",({target})=>{
+        target.style.backgroundColor ="red"
+    })
+})
