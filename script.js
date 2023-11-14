7f79d66c0db6319c6665296edd4cfb0be398ce4c
ka1rakan <hsuhankaraca@gmail.com>
2023-11-14 18:54:53 +0300

    make website react to the changes made in the slider

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const container = document.querySelector("#container");
-
 const slider = document.querySelector("#myRange");
 const output = document.getElementById("demo");
 
-output.innerText = slider.value; // Display the default slider value
-let boxPerRow = slider.value;
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerText = this.value;
-  boxPerRow = this.value;
+function deleteChildren(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild)
+    }
 }
 
+output.innerText = slider.value; // Display the default slider value
+let boxPerRow = slider.value;
 for(let i=0; i<boxPerRow**2; i++){
     const div = document.createElement("div");
     div.style.width=`${100/boxPerRow}%`;
@@ -18,20 +17,46 @@ for(let i=0; i<boxPerRow**2; i++){
     container.appendChild(div);
 }
 
-const dividers = document.querySelectorAll("#container div")
-
+let dividers = document.querySelectorAll("#container div")
 dividers.forEach((div)=>{
     div.addEventListener("mouseover",({target})=>{
         target.style.backgroundColor ="rgba(108, 108, 108, 0.94)"
     })
 })
 
-const reset = document.querySelector("#reset");
+let reset = document.querySelector("#reset");
 reset.addEventListener("click", (e)=>{
     dividers.forEach((div)=>{
         div.style.backgroundColor = "rgba(202, 202, 202, 0.94)"
     })
 })
+
+
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerText = this.value;
+    boxPerRow = this.value;
+    deleteChildren(container)
+    for(let i=0; i<boxPerRow**2; i++){
+        const div = document.createElement("div");
+        div.style.width=`${100/boxPerRow}%`;
+        div.style.height = `${100/boxPerRow}%`
+        container.appendChild(div);
+    }
+    let dividers = document.querySelectorAll("#container div")
+    dividers.forEach((div)=>{
+        div.addEventListener("mouseover",({target})=>{
+            target.style.backgroundColor ="rgba(108, 108, 108, 0.94)"
+        })
+    })
+    let reset = document.querySelector("#reset");
+    reset.addEventListener("click", (e)=>{
+        dividers.forEach((div)=>{
+            div.style.backgroundColor = "rgba(202, 202, 202, 0.94)"
+        })
+    })
+}
 
 /**
              ('--..--`)
