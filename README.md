99cc7f43b5ebbcb8248146ce219cecf5e638c7b7
ka1rakan <kairaskaraca@gmail.com>
2024-05-20 08:40:57 +1000

    Add boxes to container

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+Browser version of a sketchpad
+made by kairakan
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 64; i++) {
+let boxPerRow = 16;
+for (let i = 0; i < Math.pow(boxPerRow, 2); i++) {
     let box = document.createElement("div");
     box.className = "box";
+
+    box.style.width = (450 / boxPerRow).toString() + "px";
+    box.style.height = (450 / boxPerRow).toString() + "px";
     container.appendChild(box);
 }
