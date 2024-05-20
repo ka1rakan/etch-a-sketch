17da289c3134f539fd0752a9e0f0f3142925d105
ka1rakan <kairaskaraca@gmail.com>
2024-05-20 12:31:55 +1000

    add slider event listener

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const container = document.querySelector(".container");
 
-let boxPerRow = 16;
-for (let i = 0; i < Math.pow(boxPerRow, 2); i++) {
-    let box = document.createElement("div");
-    box.className = "box";
+let boxPerRow = 25;
+function createBoxes(boxPerRow) {
+    for (let i = 0; i < Math.pow(boxPerRow, 2); i++) {
+        let box = document.createElement("div");
+        box.className = "box";
 
-    box.style.width = (450 / boxPerRow).toString() + "px";
-    box.style.height = (450 / boxPerRow).toString() + "px";
-    container.appendChild(box);
+        box.style.width = (450 / boxPerRow).toString() + "px";
+        box.style.height = (450 / boxPerRow).toString() + "px";
+        container.appendChild(box);
+    }
 }
+function removeBoxes() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+const slider = document.querySelector(".slider");
+slider.addEventListener("change", (e) => {
+    removeBoxes();
+    createBoxes(e.target.value);
+});
+createBoxes(slider.value);
+
+
